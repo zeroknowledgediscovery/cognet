d3c7346f444c5c011c4bc2e788f73aac0a1dfc0f
David Yang <davidyangnyc@midway2-login1.rcc.local>
2021-09-22 21:44:06 -0500

    updated

---
+++ examples/mpi_tmp/GSS_mpi_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+YEARS='2018'
+# nodes requested
+NODES=4
+# time requested
+T=14
+NUM='all'
+LAUNCH='../mpi_launcher.sh'
+
+for yr in `echo $YEARS`
+do
+	echo $yr
+	./GSS_mpi_setup.sh $yr $NODES $NUM tmp_"$yr"
+	$LAUNCH -P tmp_"$yr" -F -T $T -N "$NODES" -C 28 -p broadwl -J ACRDALL_"$yr" -M 56
+done
+rm tmp_"$yr"*
--- examples/mpi_tmp/GSS_mpi_setup.sh
+++ examples/mpi_tmp/GSS_mpi_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 YEAR=$1
+
+if [ $# -gt 1 ] ; then
 	NODES=$2
 else
 	NODES=3
@@ -13,13 +15,12 @@ if [ $# -gt 3 ] ; then
 	PROG=$4
 else
 	PROG=$(tty)
-
-
 fi
+
 NUMPROC=`expr 28 \* $NODES`
 echo "module load midway2" >> $PROG
 echo "module unload python" >> $PROG
 echo "module unload openmpi" >> $PROG
 echo "module load python/anaconda-2020.02" >> $PROG
 echo "module load mpi4py" >> $PROG
-echo "date; mpiexec -n "$NUMPROC" python3 -Xy="$YEAR" -XN="$NUM" -m mpi4py.futures tmp_dmat.py; date"  >> $PROG
+echo "date; mpiexec -n "$NUMPROC" python3 -m mpi4py.futures GSS_cognet.py; date"  >> $PROG
